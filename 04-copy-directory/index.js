be96d0806cfe6f43099f69f3a1dd8bd95d453c86
Vladislav Novitskiy <no-vlad@yandex.ru>
2024-01-23 19:05:16 +0300

    fix: copy-directory add remove dir before copy

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,31 +5,36 @@ const srcDir = path.resolve(__dirname, 'files');
 const destDir = path.resolve(__dirname, 'files-copy');
 
 async function copyFolder(fromDir, toDir) {
-  fs.readdir(fromDir, { withFileTypes: true }, function (err, files) {
-    if (err) {
-      throw new Error('Error reading directory: ' + err.message);
+  fs.rm(toDir, { recursive: true, force: true }, (error) => {
+    if (error) {
+      console.log(error);
     }
-    fs.mkdir(toDir, { recursive: true }, (err) => {
+    fs.readdir(fromDir, { withFileTypes: true }, function (err, files) {
       if (err) {
-        throw new Error('Error creating directory: ' + err.message);
+        throw new Error('Error reading directory: ' + err.message);
       }
-    });
-
-    if (files.length > 0) {
-      files.forEach((file) => {
-        if (file.isDirectory()) {
-          const subDir = file.name;
-          copyFolder(
-            path.resolve(fromDir, subDir),
-            path.resolve(toDir, subDir),
-          );
-        } else {
-          fs.createReadStream(path.resolve(fromDir, file.name)).pipe(
-            fs.createWriteStream(path.resolve(toDir, file.name)),
-          );
+      fs.mkdir(toDir, { recursive: true }, (err) => {
+        if (err) {
+          throw new Error('Error creating directory: ' + err.message);
         }
       });
-    }
+
+      if (files.length > 0) {
+        files.forEach((file) => {
+          if (file.isDirectory()) {
+            const subDir = file.name;
+            copyFolder(
+              path.resolve(fromDir, subDir),
+              path.resolve(toDir, subDir),
+            );
+          } else {
+            fs.createReadStream(path.resolve(fromDir, file.name)).pipe(
+              fs.createWriteStream(path.resolve(toDir, file.name)),
+            );
+          }
+        });
+      }
+    });
   });
 }
 
